9892180e439d2ce335be2bff56328b0ca7538d9d
nadhim <nadhimregent@gmail.com>
2025-02-26 19:19:13 +0530

    add next functionality

---
+++ 4.DOM/DOM mini-projects/Stepper/script.js
@@ -0,0 +1,56 @@
+let currentStep = 1
+
+const pageMessageObject = {
+  1: "Add Contact Details for further Information",
+  2: "Add Shipping Address for Sucessfull Delivery",
+  3: "Complete Payment to Complete Order",
+  4: "Ready To Get Deliverd",
+  5: "Happy Happy , Order Got Deliverd ",
+}
+
+const previousButton = document.querySelector(".previous")
+const nextButton = document.querySelector(".next")
+
+nextButton.addEventListener("click", nextEvent)
+
+function nextEvent(eventObj) {
+
+    if ( currentStep > 4 ){
+        // Chnaging Message Content
+        const mesgDiv = document.querySelector(".message")
+        mesgDiv.innerText = pageMessageObject[5]
+        return 
+    }
+
+    if (currentStep == 3) {
+    document.querySelector(".next").innerText = "Finish"
+    }
+
+    // Chnaging Message Content
+    const mesgDiv = document.querySelector(".message")
+    mesgDiv.innerText = pageMessageObject[currentStep + 1 ]
+
+
+
+    // Chnaging Curr Step Color 
+    const currStepNumberDiv = document.querySelectorAll(`.stepper .step-number`)[currentStep - 1]
+    currStepNumberDiv.querySelector("div").innerText = "O"
+    currStepNumberDiv.classList.remove("blue-bg")
+    currStepNumberDiv.classList.add("green-bg")
+
+    if( currentStep < 4 ){
+        currStepNumberDiv.nextElementSibling.style.borderColor = "green"
+    
+        // Changing next step color 
+        const nextStepNumberDiv = document.querySelectorAll(`.stepper .step-number`)[currentStep]
+        nextStepNumberDiv.classList.add("blue-bg")
+    }
+
+    currentStep++  
+}
+
+previousButton.addEventListener("click", prevEvent)
+
+function prevEvent(eventObj) {
+  console.log("prev")
+}
